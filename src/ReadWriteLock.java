bf6e7ec1fef600ea9fd1882b9ff35a8b2d7ebb20
Shreya Srinivas <shreya@Shreyas-MacBook-Air.local>
2015-12-05 21:49:24 -0500

    added read/write lock class

---
+++ src/ReadWriteLock.java
@@ -0,0 +1,27 @@
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class ReadWriteLock {
+	
+	private ReentrantReadWriteLock[] locks;
+	
+	public ReadWriteLock(int numLocks) {
+		locks = new ReentrantReadWriteLock[numLocks];
+	}
+	
+	public void lockRead(int address) {
+		locks[address].readLock().lock();
+	}
+	
+	public void unlockRead(int address) {
+		locks[address].readLock().unlock();
+	}
+	
+	public void lockWrite(int address) {
+		locks[address].writeLock().lock();
+	}
+	
+	public void unlockWrite(int address) {
+		locks[address].writeLock().unlock();
+	}
+
+}
