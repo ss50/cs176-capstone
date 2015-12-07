9d42d79bb6a9d67c9c5d46b114c84db98261a006
Dan Haugh <daniel_haugh@brown.edu>
2015-12-06 22:45:17 -0500

    changed numInFlightToAtomicInteger. Adding callback function next

--- src/Dispatcher.java
+++ src/Dispatcher.java
@@ -1,7 +1,7 @@
 import java.util.concurrent.atomic.*;
 
 public class Dispatcher implements Runnable {
-	
+
 	PaddedPrimitiveNonVolatile<Boolean> done;
 	PaddedPrimitiveNonVolatile<Integer> inFlight;
 	PaddedPrimitive<Boolean> memFence;
@@ -9,11 +9,14 @@ public class Dispatcher implements Runnable {
 	private int numAddresses;
 	private ConfigPacketHandler configHandler;
 	private DataPacketHandler dataHandler;
-	private int numInFlight = 0;
-	
-	public Dispatcher(PaddedPrimitiveNonVolatile<Boolean> done, PaddedPrimitiveNonVolatile<Integer> numInFlight, PaddedPrimitive<Boolean> memFence, int numAddressesLog, PacketGenerator gen) {
+	private AtomicInteger numInFlight = new AtomicInteger(0);
+
+	public Dispatcher(PaddedPrimitiveNonVolatile<Boolean> done,
+			PaddedPrimitiveNonVolatile<Integer> numInFlight,
+			PaddedPrimitive<Boolean> memFence, int numAddressesLog,
+			PacketGenerator gen) {
 		this.done = done;
-		this.inFlight = numInFlight;
+//		this.inFlight = numInFlight;
 		this.memFence = memFence;
 		this.numAddresses = (int) Math.pow(2, numAddressesLog);
 		this.pktGen = gen;
@@ -28,22 +31,22 @@ public class Dispatcher implements Runnable {
 			for (int i = 0; i < numAddresses; i++) {
 				Packet packet = pktGen.getPacket();
 				// only 256 packets can be in flight at once
-				while (numInFlight > 256 && !done.value) {;}
-				numInFlight++;
-				inFlight.value++;
+				while (numInFlight.get() > 256 && !done.value) {}
+				numInFlight.addAndGet(1);
+//				inFlight.value++;
 				switch (packet.type) {
-					case ConfigPacket:
-						// send to config thread pool
-						configHandler.handlePacket(packet);
-						break;
-					case DataPacket:
-						// send to data thread pool
-						dataHandler.handlePacket(packet);
-						break;
+				case ConfigPacket:
+					// send to config thread pool
+					configHandler.handlePacket(packet);
+					break;
+				case DataPacket:
+					// send to data thread pool
+					dataHandler.handlePacket(packet);
+					break;
 				}
 			}
 		}
-		
+
 	}
 
 }
