93d7c4975879fe1c2cf86dae33d21d020f2cf934
Shreya Srinivas <shreya@Shreyas-MacBook-Air.local>
2015-12-06 20:38:06 -0500

    dispatcher should only have 256 packets in flight

--- src/Dispatcher.java
+++ src/Dispatcher.java
@@ -3,14 +3,17 @@ import java.util.concurrent.atomic.*;
 public class Dispatcher implements Runnable {
 	
 	PaddedPrimitiveNonVolatile<Boolean> done;
+	PaddedPrimitiveNonVolatile<Integer> inFlight;
 	PaddedPrimitive<Boolean> memFence;
 	private PacketGenerator pktGen;
 	private int numAddresses;
 	private ConfigPacketHandler configHandler;
 	private DataPacketHandler dataHandler;
+	private int numInFlight = 0;
 	
-	public Dispatcher(PaddedPrimitiveNonVolatile<Boolean> done, PaddedPrimitive<Boolean> memFence, int numAddresses, PacketGenerator gen) {
+	public Dispatcher(PaddedPrimitiveNonVolatile<Boolean> done, PaddedPrimitiveNonVolatile<Integer> numInFlight, PaddedPrimitive<Boolean> memFence, int numAddresses, PacketGenerator gen) {
 		this.done = done;
+		this.inFlight = numInFlight;
 		this.memFence = memFence;
 		this.numAddresses = numAddresses;
 		this.pktGen = gen;
@@ -24,6 +27,10 @@ public class Dispatcher implements Runnable {
 		while (!done.value) {
 			for (int i = 0; i < numAddresses; i++) {
 				Packet packet = pktGen.getPacket();
+				// only 256 packets can be in flight at once
+				while (numInFlight > 256 && !done.value) {;}
+				numInFlight++;
+				inFlight.value++;
 				switch (packet.type) {
 					case ConfigPacket:
 						// send to config thread pool
