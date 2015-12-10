a1b47cd02e4155f6eaa11fc5a5d481dabf7a6b07
Shreya Srinivas <shreya@Shreyas-MacBook-Air.local>
2015-12-10 03:20:00 -0500

    added size checks for ConcurrentQueue

--- src/ConcurrentQueue.java
+++ src/ConcurrentQueue.java
@@ -3,8 +3,10 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 @SuppressWarnings("hiding")
 public class ConcurrentQueue{
 	static final ConcurrentLinkedQueue<PacketCallbackBundle> queue[] = new ConcurrentLinkedQueue[ParallelFirewall.NUM_DISPATCH_THREADS];
+	private static int queueSize;
 	
 	public static void initialize(){
+		queueSize = 256; // can be changed for testing
 		for (int i = 0; i < queue.length; i++) {
 			queue[i] = new ConcurrentLinkedQueue<PacketCallbackBundle>();
 		}
@@ -13,6 +15,9 @@ public class ConcurrentQueue{
 
 	public static boolean enqueue(int index, PacketCallbackBundle item) {
 		// TODO Auto-generated method stub
+		if (queue[index].size() > 256) {
+			return false;
+		}
 		return queue[index].offer(item);
 	}
 
