4d37a50ad1f17f8e25dd8c0111ab142231a75fc6
Shreya Srinivas <shreya@Shreyas-MacBook-Air.local>
2015-12-10 21:50:52 -0500

    got rid of SerialQueueFirewall

--- src/Firewall.java
+++ src/Firewall.java
@@ -77,45 +77,6 @@ class SerialFirewall {
 	}
 }
 
-class SerialQueueFirewall {
-	public static void main(String[] args) {
-		final int numMilliseconds = Integer.parseInt(args[0]);
-		final int numSources = Integer.parseInt(args[1]);
-
-		final int numAddressesLog = Integer.parseInt(args[2]);
-		final int numTrainsLog = Integer.parseInt(args[3]);
-		final double meanTrainSize = Double.parseDouble(args[4]);
-		final double meanTrainsPerComm = Double.parseDouble(args[5]);
-		final int meanWindow = Integer.parseInt(args[6]);
-		final int meanCommsPerAddress = Integer.parseInt(args[7]);
-		final int meanWork = Integer.parseInt(args[8]);
-		final double configFraction = Double.parseDouble(args[9]);
-		final double pngFraction = Double.parseDouble(args[10]);
-		final double acceptingFraction = Double.parseDouble(args[11]);
-
-		StopWatch timer = new StopWatch();
-		PacketGenerator pktGen = new PacketGenerator(numAddressesLog,
-				numTrainsLog, meanTrainSize, meanTrainsPerComm, meanWindow,
-				meanCommsPerAddress, meanWork, configFraction, pngFraction,
-				acceptingFraction);
-
-		Fingerprint residue = new Fingerprint();
-		// ...
-		// allocate and initialize bank of numSources Lamport queues
-		// each with depth queueDepth
-		// they should throw FullException and EmptyException upon those
-		// conditions
-		// ...
-		int totalPackets = (int) Math.pow(2, numAddressesLog);
-		AtomicQueue packetQueues[] = new AtomicQueue[numSources];
-
-		for (int i = 0; i < numSources; i++) {
-			packetQueues[i] = new AtomicQueue<Packet>();
-		}
-
-		// TODO: implement logic for dispatching config and data packets here
-	}
-}
 
 class FirewallTest {
 	public static void main(String[] args) {
